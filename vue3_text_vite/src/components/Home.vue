7ec08fad1116e7518f24b3f24a852a37f8033364
oldBe <yngxtt@163.com>
2022-05-07 00:52:24 +0800

    get song url

--- vue3_text_vite/src/components/Home.vue
+++ vue3_text_vite/src/components/Home.vue
@@ -1,21 +1,18 @@
 <template>
   <h1>Home组件</h1>
   <div>
-    <div>
+    <div class="cover">
       <input type="text" v-model="keyWord"><button @click="getSongs">搜索</button>
-      <div v-for="(item,index) in stare.song" @click="getSongUrl(item.id)">
-        <img :src="item.artists[0].img1v1Url" :alt="item.name">
-        <span>{{item.name}}</span>
-        <span>{{item.id}}</span>
+      <div v-for="(item,index) in stare.song" @click="getSongUrl(item.id)" class="item">
+          <div>
+            <img :src="item.artists[0].img1v1Url" :alt="item.name">
+            <span>{{item.name}}</span>
+          </div>
+          <span>{{item.id}}</span>
       </div>
       <button @click="addName">TEST</button>
     </div>
-    <div class="play">
-        <audio controls="controls">
-          <source :src="songUrl" type="audio/mp3" />
-        </audio>
-        {{songUrl}}
-    </div>
+        <iframe class="play" frameborder="no" border="0" marginwidth="10" marginheight="0" width=100% height=86 :src="stare.songUrl"></iframe>
   </div>
 </template>
 
@@ -81,9 +78,9 @@ import {storeToRefs} from 'pinia'
 
       const getSongUrl = async (id) => {
         const result = await songIdSearch(id)
-        stare.songUrl = result.data
-        songUrl =  stare.songUrl.data[0].url
-        console.log( stare.songUrl.data[0].url )
+        stare.songByUrlData = result.data
+        stare.songUrl =  stare.songByUrlData.data[0].url
+        console.log( stare.songByUrlData.data[0].url )
     }
     
 
@@ -93,8 +90,8 @@ import {storeToRefs} from 'pinia'
 
 <style>
 img {
-  width: 50px;
-  height: 50px;
+  width: 30px;
+  height: 30px;
   border-radius: 25px;
 }
 span {
@@ -105,100 +102,28 @@ span {
   left: 0;
   bottom: 0;
   right: 0;
-  top: 80%;
   background-color: aqua;
 }
-
-</style>
-
-<script  lang="ts" setup>
-import { getBeeList , songIdSearch } from '../apis/api'
-import { ref ,reactive, toRefs, onMounted } from 'vue'
-import { useTestStore } from '../store/index'
-import {storeToRefs} from 'pinia'
-   
-
-    let keyWord = ref('孤勇者')
-    let songUrl = ref('')
-    
-    const  key =  keyWord.value
-    // const Data = JSON.stringify(data)
-
-    const stare = useTestStore()
-    // const {name, age} = stare  //结构没有响应式  使用storeToRefs可以解决
-    
-    const {name, age} = storeToRefs(stare) 
-    const addName = () => {
-      // 修改state方法
-      //1. stare.name = stare.name + '-'
-      //2.
-      // stare.$patch((state)=>{
-      //     stare.name = stare.name + '-'
-      // })
-      // 3.
-      // stare.$patch({name:'hello'})
-      // 4.
-      // stare.$state = {name:'hello'}
-      //5.
-      // stare.setTestName('大漂亮')
-
-      // alert(name+','+age)
-
-      stare.setTestName()
-    }
-
-   
-    
-
-    const go =   () => {
-     return new Promise((resolve)=>{
-       const resvt = getBeeList(keyWord.value)
-       resolve(resvt)
-     }).catch((err)=>{
-       console.log(err)
-     })
-      // console.log(Songs.data.result.songs[1].name)
-      // Data = Songs.data.result
-      // onMounted() {
-
-      // }
-      
-    }
-    // console.log(Songs)
-    const getSongs = async () => {
-        const result = await go()
-        stare.song = result.data.result.songs
-        console.log( stare.song)
-    }
-
-      const getSongUrl = async (id) => {
-        const result = await songIdSearch(id)
-        stare.songUrl = result.data
-        songUrl =  stare.songUrl.data[0].url
-        console.log( stare.songUrl.data[0].url )
-    }
-    
-
-  
-
-</script>
-
-<style>
-img {
-  width: 50px;
-  height: 50px;
-  border-radius: 25px;
+.cover {
+  display: block;
+ margin: 0 auto;
 }
-span {
-  font-size: 50px;
+.item {
+  width: 600px;
+  display: flex;
+  justify-content: space-between;
+  height: 30px;
+  background-color: #666;
+  margin: 10px 0;
+   line-height: 30px;
 }
-.play {
-  position: fixed;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  top: 80%;
-  background-color: aqua;
+.item span{
+  font-size: 20px;
+  line-height: 30px;
+}
+.item div span{
+  font-size: 20px;
+  line-height: 30px;
 }
 
 </style>
--- vue3_text_vite/src/store/index.ts
+++ vue3_text_vite/src/store/index.ts
@@ -26,7 +26,8 @@ export const useTestStore = defineStore(Names.TEST,{
             name: 'zhangSan',
             age: 18,
             song: null,
-            songUrl: null
+            songByUrlData: null,
+            songUrl: 'http://m701.music.126.net/20220507004020/968e802f497ad2d16ca557456f02326f/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/14096429529/2bea/211b/0672/43be1ec076b8d7d7dc457549274e1d5f.mp3'
         }
     },
 
