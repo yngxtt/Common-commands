8390847644a90568a7d9f96b90b156e61ff96140
oldBe <yngxtt@163.com>
2022-06-21 01:46:18 +0800

    递归

--- ele-ui/src/components/test.vue
+++ ele-ui/src/components/test.vue
@@ -14,7 +14,12 @@ export default {
            },
            testData: {
             a: '112 23  ',
-            b: 'Ab       '
+            b: 'Ab       ',
+            c: {
+                c1: ' 11   ',
+                c2: '222    '
+            },
+            d: ['   11  ', ' 3333  ', '  43534535 ']
            },
            str: '  1243   '
         }
@@ -31,20 +36,29 @@ export default {
         },
         async test() {
             await this['trimTest'](this.str)
-            console.log(this.str)
+            //  console.log(await this['trimTest'](this._data))
+            // console.log(this._data)
+             console.log(this.str)
         },
         trimTest(val) {
-            console.log(this[val])
-            if(typeof val === 'string') {
-               this[val] = val.trim().trimEnd()
-               val =  this[val]
-            } else {
+            // // console.log(this[val])
+            // if(typeof val === 'string') {
+            //    this._data[val] = val.trim().trimEnd()
+            //    val =  this[val]
+            // } else {
+                // if ( typeof val === 'string' ) {
+                //     this._data[val] = val.trim().trimEnd()
+                //     console.log(this)
+                //     return
+                // }
                 for(const key in val) {
                     if(typeof  val[key] === 'string') {
                         val[key] = val[key].trim().trimEnd()
+                    } else if (typeof val[key] === 'object') {
+                        this.trimTest(val[key])
                     }
                 }
-            }
+            // }
             return val
         }
     }
